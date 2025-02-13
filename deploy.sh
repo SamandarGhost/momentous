68ad852e6c24f1d6310ea520ffab01e8016c5745
Sam <kimjeyeon03@icloud.com>
2025-02-13 22:12:29 +0900

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #PRODUCTION
-# git reset --hard
-# git checkout master
-# git pull origin master
-
+git reset --hard
+git checkout master
+git pull origin master
 npm i
 npm run build
 pm2 start process.config.js --env production
